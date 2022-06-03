97c1f55e6548553a7ab369b84fad74eacb2f12c5
Paulo Domingues <90154095+PauloCaramigo@users.noreply.github.com>
2022-06-03 10:50:28 -0300

    Atualizando o Desafio010

--- Desafios/Desafio010.py
+++ Desafios/Desafio010.py
@@ -2,6 +2,7 @@
 # Crie um programa que leia quanto uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
 # US$ 1.00 = R$ 3,27
 
-valor = float(input('Quantos reais vc tem na carteira? '))
+valor = float(input('Quantos reais vc tem na carteira? R$'))
 
-print('O valor que você tem em dolares é {:.2f}'.format(valor*0.305))
+print('O valor que você tem em dolar é US${:.2f}'.format(valor/4.8))
+print('O valor que você tem em rublo russo é {:.2f}₽'.format(valor/0.076))
