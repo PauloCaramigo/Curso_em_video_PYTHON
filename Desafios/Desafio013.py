0e4390420f2624f0f5895741038a35dc73a2b2d6
Paulo Domingues <90154095+PauloCaramigo@users.noreply.github.com>
2022-06-03 11:21:04 -0300

    Atualização exercício 13
    Atualizando o exercício 13 utilizando um algoritmo matemático de porcentagem.

--- Desafios/Desafio013.py
+++ Desafios/Desafio013.py
@@ -3,4 +3,9 @@
 
 salario = float(input('Qual é o seu salário? '))
 
-print('\nPARABÉNS!!!\nVocê ganhou um aumento de salário!\nSeu novo salário é de {}'.format(salario*1.15))
+print('\nPARABÉNS!!!\nVocê ganhou um aumento de salário de 15%!\nSeu novo salário é de R${:.2f}'.format(salario*1.15))
+print('\nPARABENS!!!\nVocê ganhou um aumento de salário de 15%!\nSeu novo salário é de R${:.2f}'
+      .format(salario + (salario*15/100)))
+print('\nPARABÉNS!!!\nVocê ganhou um aumento de salário de 25%!\nSeu novo salário é de R${:.2f}'
+      .format(salario + (salario*25/100)))
+
