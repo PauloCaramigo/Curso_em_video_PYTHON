9305860dc91da4f0686969f8de3a3f5a3a1c76d8
Paulo Domingues <90154095+PauloCaramigo@users.noreply.github.com>
2022-06-03 12:03:42 -0300

    Upload do exercício 14

---
+++ Desafios/Desafio014.py
@@ -0,0 +1,7 @@
+# ENUNCIADO
+# Converta a temperatura Celsius para Fahrenheit
+
+celsius = float(input('Qual é a temperatura em celsius? '))
+fahrenheit = (celsius*9/5) + 32
+
+print('A temperatura {:.1f}Cº em fahrenheit é de {:.1f}Fº'.format(celsius, fahrenheit))
