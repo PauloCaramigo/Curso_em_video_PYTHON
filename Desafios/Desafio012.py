433188577886b9c5d1af09861d905a3dbbf086ca
Paulo Domingues <90154095+PauloCaramigo@users.noreply.github.com>
2022-06-03 11:07:09 -0300

    Atualização exercício 12
    Aplicando conceito matemático de porcentagem.

--- Desafios/Desafio012.py
+++ Desafios/Desafio012.py
@@ -4,3 +4,6 @@
 preco = float(input('Qual o valor do produto? '))
 
 print('O novo valor do seu produto com 5% de desconto é de {:.2f}!'.format(preco*0.95))
+print('O novo valor do seu produto com 5% de desconto é de {:.2f}!'.format(preco - (preco*5/100)))
+print('O novo valor do seu produto com 10% de desconto é de {:.2f}!'.format(preco - (preco*10/100)))
+print('O novo valor do seu produto com 27% de desconto é de {:.2f}!'.format(preco - (preco*27/100)))
